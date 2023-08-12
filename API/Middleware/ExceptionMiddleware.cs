5c794d4194ff19368002eeb81e117da32cdabf84
Paramanand Rajbhar <dotnetetc@gmail.com>
2023-08-12 17:58:52 +0530

    End of section 5

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Http;
 using System.Net;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
-
+using System.Text.Json;
+using System.Runtime.InteropServices;
 
 namespace API.Middleware
 {
@@ -35,7 +36,10 @@ namespace API.Middleware
                 ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString()) 
                 : new ApiException((int)HttpStatusCode.InternalServerError);
 
-               
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+               var json =   JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json); 
 
             }
         }
