9e79c4ba715747283c416a5e0fa088c885c5903d
Paramanand Rajbhar <dotnetetc@gmail.com>
2023-08-13 13:28:30 +0530

    End of section 6

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Data
         private readonly StoreContext _context;
         public GenericRepository(StoreContext context)
         {
-            _context = context ;
+            _context = context;
         }
         public async Task<T> GetByIdAsync(int id)
         {
@@ -30,10 +30,17 @@ namespace Infrastructure.Data
         {
             return await ApplySpecification(spec).ToListAsync();
         }
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
 
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
-            return SpecificationEvaluater<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
+            return SpecificationEvaluater<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
+
     }
+       
 }
